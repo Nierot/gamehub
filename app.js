ad20eb6ac13d8074624eaa2e1ff1dd5ff06da429
Nierot <56843328+Nierot@users.noreply.github.com>
2020-08-10 13:28:23 +0200

    starting instances

--- app.js
+++ app.js
@@ -2,30 +2,31 @@ const app = require('express')();
 const settings = require('./settings.json');
 const bodyParser = require('body-parser');
 const discord = require('./utils/discord');
-const { Worker } = require('worker_threads');
+const threads = require('./utils/threads');
+const cors = require('cors');
 
 let workers = {};
 
-app.get('/', (req, res) => {
-    res.render('pages/index');
-});
-
-app.get('/login', (req, res) => {
-    res.render('pages/login');
-})
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/login', (req, res) => res.render('pages/login'))
 
 app.get('/start/*', async (req, res) => {
     let game = req.url.split('/')[2];
-    if (!settings.available_games.includes(game)) return res.send('whats that');
-
-    res.send(`Launching an instance of ${game}`);
-    let worker = new Worker('./test.js');
-    worker.on('message', msg => {
-        workers[msg.toString()] = worker;
-        console.log(workers);
-    });
-    worker.on('error', err => console.error(err));
-    worker.on('exit', exitCode => console.log(`Worker has stopped with exit code: ${exitCode}`));
+    if (!settings.available_games.includes(game)) return res.sendStatus(400);
+    console.log(`Starting an instance of ${game}`);
+    threads.spawnInstance(settings.game_paths[game])
+        .then(obj => {
+            workers[obj.port] = {
+                worker: obj.worker,
+                dateCreated: new Date()
+            }
+            res.send(obj.port); // Reverse proxy
+            console.log(workers);
+        })
+        .catch(err => {
+            res.sendStatus(500);
+            console.error(err);
+        })
 });
 
 app.get('/discord/callback/success', async (req, res) => {
@@ -37,17 +38,11 @@ app.get('/discord/callback/success', async (req, res) => {
     })
 })
 
-app.get('/discord/callback', (req, res) => {
-    res.sendFile('static/discord_redirect.html', {
-        root: '.'
-    });
-})
-
-app.get('/discord', (req, res) => {
-    res.redirect(settings.discord_oauth_url);
-})
+app.get('/discord/callback', (req, res) => res.sendFile('static/discord_redirect.html', { root: '.' }))
+app.get('/discord', (req, res) => res.redirect(settings.discord_oauth_url))
 
 app.use(bodyParser);
+app.use(cors);
 app.set('view engine', 'ejs');
 
 app.listen(settings.port, () => {
